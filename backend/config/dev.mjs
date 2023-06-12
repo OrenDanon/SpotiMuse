233c7bd05f8968233df176f392109c282634601a
Raz Simchi <razsimchi@gmail.com>
2023-06-12 14:59:55 +0300

    add backend

---
+++ backend/config/dev.mjs
@@ -0,0 +1,7 @@
+export default {
+  dbURL: 'mongodb+srv://razsimchi:iQMz4kQlxrgynLkJ@spotify.a0t9bct.mongodb.net/?retryWrites=true&w=majority',
+  dbName: 'spotify'
+}
+
+
+// mongodb+srv://razsimchi:<password>@spotify.a0t9bct.mongodb.net/
